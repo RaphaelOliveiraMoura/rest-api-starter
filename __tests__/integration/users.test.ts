b32947380d06793cf734974fd89b944ca8446351
Raphael <raphaeldeoliveiramoura@hotmail.com>
2019-05-29 14:23:13 -0300

    supertest wrapper removed

--- __tests__/integration/users.test.ts
+++ __tests__/integration/users.test.ts
@@ -1,6 +1,8 @@
-import request from "../../source/wrappers/supertestsWrapper";
+import request from "supertest";
 import { application } from "../../source/app";
 import databaseConfiguration from '../configurations/sequelize.configuration';
+
+
 
 beforeAll(async () => {
     await application.syncDatabase(databaseConfiguration);
@@ -8,18 +10,34 @@ beforeAll(async () => {
 
 describe('list users - GET /users', () => {
     it('should return a list of users and 200 status', async () => {
-        const response = await request(application).get('/users');
+        const response = await request(application.express)
+            .get('/api/v1/users')
+            .set({
+                'Accept': 'application/json',
+                'Authorization': 'token'
+            })
+
         expect(response.status).toEqual(200);
         expect(response.text).toContain([]);
-
     });
 });
 
 describe('create user - POST /users', () => {
     it('should response a sucess message and status 200 without errors when pass the correct args', async () => {
 
-        const response = await request(application)
-            .post('/users', { name: 'john', email: 'john@gmail.com', password: '123456' });
+        const user = {
+            name: 'john',
+            email: 'john@gmail.com',
+            password: '123456'
+        }
+
+        const response = await request(application.express)
+            .post('/api/v1/users')
+            .set({
+                'Accept': 'application/json',
+                'Authorization': 'token'
+            })
+            .send(user);
 
         expect(response.status).toEqual(200);
 
@@ -27,8 +45,18 @@ describe('create user - POST /users', () => {
 
     it('should response a error message with status 204 when not pass the corrects args', async () => {
 
-        const response = await request(application)
-            .post('/users', { name: 'john' });
+        const user = {
+            name: 'john',
+            email: 'john@gmail.com'
+        }
+
+        const response = await request(application.express)
+            .post('/api/v1/users')
+            .set({
+                'Accept': 'application/json',
+                'Authorization': 'token'
+            })
+            .send(user);
 
         expect(response.status).toEqual(400);
         expect(response.text).toContain('errors');
