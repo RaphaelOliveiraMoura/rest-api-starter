9f7d249b129e3d5c084c2b84aa9f61af2f03d511
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-06-18 15:31:01 -0300

    app config minified

--- source/configuration/application.configuration.ts
+++ source/configuration/application.configuration.ts
@@ -1,23 +1,7 @@
 require('dotenv').config();
-
-type ConfigurationInterface = {
-    'environment': string,
-    'host': string,
-    'port': string|number,
-    'endpoint': string
+export default {
+    'environment': process.env.ENV || 'development',
+    'host': process.env.HOST || 'localhost',
+    'port': process.env.PORT || 3003,
+    'endpoint': process.env.ENDPOINT || '/api/v1'
 }
-
-class Configuration{
-    public configurantions: ConfigurationInterface;
-
-    constructor(){
-        this.configurantions = {
-            'environment': process.env.ENV || 'development',
-            'host': process.env.HOST || 'localhost',
-            'port': process.env.PORT || 3003,
-            'endpoint': process.env.ENDPOINT || '/api/v1'
-        }
-    }
-}
-
-export default new Configuration().configurantions;
