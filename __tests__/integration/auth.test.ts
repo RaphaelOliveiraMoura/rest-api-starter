c7545673b84dda9339bfb257ed31d6014decdc8e
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-06-26 14:05:11 -0300

    start update and delete tests

--- __tests__/integration/auth.test.ts
+++ __tests__/integration/auth.test.ts
@@ -14,37 +14,37 @@ beforeAll(async () => {
     expect(response.status).toEqual(200);
 })
 
+describe('POST /authenticate', async () => {
+    await it('should authenticate a user with the corrects args', async () => {
+        const response = await authRequester.authenticate(user);
+        expect(response.body).toHaveProperty('token');
+        expect(response.body).toHaveProperty('email');
+        expect(response.body).toHaveProperty('name');
+        expect(response.body).toHaveProperty('rules');
+        expect(response.status).toEqual(200);
+    });
 
-it('should authenticate a user with the corrects args', async () => {
-    const response = await authRequester.authenticate(user);
-    expect(response.body).toHaveProperty('token');
-    expect(response.body).toHaveProperty('email');
-    expect(response.body).toHaveProperty('name');
-    expect(response.body).toHaveProperty('rules');
-    expect(response.status).toEqual(200);
-});
+    await it('should return a error when the user pass a wrong password or email', async () => {
+        const wrongUser = {
+            email: 'wrong@gmal.com',
+            password: '123456'
+        };
+        const response = await authRequester.authenticate(wrongUser);
+        expect(response.body).toHaveProperty('error');
+        expect(response.status).toEqual(401);
+    });
+})
 
-it('should return a error when the user pass a wrong password or email', async () => {
-
-    const wrongUser = {
-        email: 'wrong@gmal.com',
-        password: '123456'
-    };
-
-    const response = await authRequester.authenticate(wrongUser);
-    expect(response.body).toHaveProperty('error');
-    expect(response.status).toEqual(401);
-});
-
-it('should be a success request passing the JWT token to validation to another route', async () => {
-    const authenticatedUser = await authRequester.authenticate(user);
-    const token = authenticatedUser.body.token;
-    const response = await userRequester.list({ token });
-    expect(response.status).toEqual(200);
-});
-
-it('should be a failure request when dont pass the correct JWT token to another route', async () => {
-    const response = await userRequester.list({ token: 'dasdasda5sd16as1d5' });
-    expect(response.body).toHaveProperty('error');
-    expect(response.status).toEqual(401);
-});
+describe('GET /users', async () => {
+    await it('should be a success request passing the JWT token to validation to another route', async () => {
+        const authenticatedUser = await authRequester.authenticate(user);
+        const token = authenticatedUser.body.token;
+        const response = await userRequester.list({ token });
+        expect(response.status).toEqual(200);
+    });
+    await it('should be a failure request when dont pass the correct JWT token to another route', async () => {
+        const response = await userRequester.list({ token: 'dasdasda5sd16as1d5' });
+        expect(response.body).toHaveProperty('error');
+        expect(response.status).toEqual(401);
+    });
+})
--- __tests__/integration/users.test.ts
+++ __tests__/integration/users.test.ts
@@ -12,36 +12,68 @@ beforeAll(async () => {
     await (<any>UserRepository).destroy({ 'truncate': { 'cascade': true } });
 })
 
-it('should response a success message and status 200 without errors when pass the correct args', async () => {
-    const response = await userRequester.create(user);
-    expect(response.status).toEqual(200);
-    expect(response.body).not.toHaveProperty('error');
-    expect(response.body).toHaveProperty('id');
-    expect(response.body).toHaveProperty('email');
-    expect(response.body).toHaveProperty('name');
-    expect(response.body).not.toHaveProperty('password');
-});
-
-it('should response a error message with status 400 when not pass the corrects args', async () => {
-
-    const wrongUser = {
-        name: 'john',
-        email: 'john@gmail.com'
-    }
-
-    const response = await userRequester.create(wrongUser);
-    expect(response.status).toEqual(400);
-    expect(response.body).toHaveProperty('error');
-});
-
-it('should return a list of users and 200 status', async () => {
-
-    const { body } = await authRequester.authenticate({
-        'email': user.email,
-        'password': user.password
+describe('POST /users', async () => {
+    await it('should response a success message and status 200 without errors when pass the correct args', async () => {
+        const response = await userRequester.create(user);
+        expect(response.status).toEqual(200);
+        expect(response.body).not.toHaveProperty('error');
+        expect(response.body).toHaveProperty('id');
+        expect(response.body).toHaveProperty('email');
+        expect(response.body).toHaveProperty('name');
+        expect(response.body).not.toHaveProperty('password');
     });
 
-    const response = await userRequester.list({ token: body.token });
-    expect(response.status).toEqual(200);
-    expect(response.text).toContain([]);
+    await it('should response a error message with status 400 when not pass the corrects args', async () => {
+
+        const wrongUser = {
+            name: 'john',
+            email: 'john@gmail.com'
+        }
+
+        const response = await userRequester.create(wrongUser);
+        expect(response.status).toEqual(400);
+        expect(response.body).toHaveProperty('error');
+    });
+
+    await it('should response a error when try create a user with a already existing email', async () => {
+
+    });
+});
+
+describe('GET /users', async () => {
+    await it('should return a list of users and 200 status', async () => {
+
+        const { body } = await authRequester.authenticate({
+            'email': user.email,
+            'password': user.password
+        });
+
+        const response = await userRequester.list({ token: body.token });
+        expect(response.status).toEqual(200);
+        expect(response.text).toContain([]);
+    });
+});
+
+describe('PUT /users', async () => {
+    await it('should update a user when pass correct args', async () => {
+
+    });
+
+    await it('should be a error request when pass wrong arguments in body to update a user', async () => {
+
+    });
+
+    await it('should be a error request when try update a user that dont exist', async () => {
+
+    });
+});
+
+describe('DELETE /users', async () => {
+    await it('should be a success request deleting a user', async () => {
+
+    });
+
+    await it('should be a error when try delete a user that dont exist', async () => {
+
+    });
 });
