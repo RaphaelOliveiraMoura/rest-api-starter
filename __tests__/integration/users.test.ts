13b72839009e2c7ffe8154137a2b70cf01e96275
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2019-07-15 15:43:07 -0300

    sync signature removed in tests describe

--- __tests__/integration/users.test.ts
+++ __tests__/integration/users.test.ts
@@ -12,8 +12,8 @@ beforeAll(async () => {
     await (<any>UserRepository).destroy({ 'truncate': { 'cascade': true } });
 })
 
-describe('POST /users', async () => {
-    await it('should response a success message and status 200 without errors when pass the correct args', async () => {
+describe('POST /users', () => {
+    it('should response a success message and status 200 without errors when pass the correct args', async () => {
         const response = await userRequester.create(user);
         expect(response.status).toEqual(200);
         expect(response.body).not.toHaveProperty('error');
@@ -23,7 +23,7 @@ describe('POST /users', async () => {
         expect(response.body).not.toHaveProperty('password');
     });
 
-    await it('should response a error message with status 400 when not pass the corrects args', async () => {
+    it('should response a error message with status 400 when not pass the corrects args', async () => {
 
         const wrongUser = {
             name: 'john',
@@ -35,13 +35,13 @@ describe('POST /users', async () => {
         expect(response.body).toHaveProperty('error');
     });
 
-    await it('should response a error when try create a user with a already existing email', async () => {
+    it('should response a error when try create a user with a already existing email', async () => {
 
     });
 });
 
-describe('GET /users', async () => {
-    await it('should return a list of users and 200 status', async () => {
+describe('GET /users', () => {
+    it('should return a list of users and 200 status', async () => {
 
         const { body } = await authRequester.authenticate({
             'email': user.email,
@@ -54,26 +54,26 @@ describe('GET /users', async () => {
     });
 });
 
-describe('PUT /users', async () => {
-    await it('should update a user when pass correct args', async () => {
+describe('PUT /users', () => {
+    it('should update a user when pass correct args', async () => {
 
     });
 
-    await it('should be a error request when pass wrong arguments in body to update a user', async () => {
+    it('should be a error request when pass wrong arguments in body to update a user', async () => {
 
     });
 
-    await it('should be a error request when try update a user that dont exist', async () => {
+    it('should be a error request when try update a user that dont exist', async () => {
 
     });
 });
 
-describe('DELETE /users', async () => {
-    await it('should be a success request deleting a user', async () => {
+describe('DELETE /users', () => {
+    it('should be a success request deleting a user', async () => {
 
     });
 
-    await it('should be a error when try delete a user that dont exist', async () => {
+    it('should be a error when try delete a user that dont exist', async () => {
 
     });
 });
